3250987f5952dba6da8581db191519eed112735e
Abdallah Deeb <abdallah@deeb.me>
2014-01-22 10:56:46 +0200

    added requirements file updated main.py to run on Linux without errors

--- main.py
+++ main.py
@@ -2,11 +2,16 @@
 import os
 import os.path
 import time
-import multiprocessing
-from multiprocessing import Process
-import androidhelper
-from android import AndroidBrowser
 import importlib
+from multiprocessing import Process
+import webbrowser
+try:
+    import androidhelper
+    from android import AndroidBrowser
+    droid = androidhelper.Android()
+except ImportError:
+    droid = None
+
 
 PROJECT_DIR = os.path.dirname(__file__)
 try:
@@ -16,17 +21,19 @@ except:
     DATABASE_FILE = os.path.join(PROJECT_DIR, 'congregation.sqlite')
 
 
-droid = androidhelper.Android()
 
 def showurl(url='http://localhost:8000'):
     time.sleep(5)
     print "[nec] Browser"
-    droid.view(url)
+    if droid is not None:
+        droid.view(url)
+    else:
+        webbrowser.open(url, 0)
 
 if not os.path.exists(DATABASE_FILE):
-    print "[nec] Syncdb" 
+    print "[nec] Syncdb"
     os.system("cd %s && python manage.py syncdb" % PROJECT_DIR)
-    
+
 
 p=Process(target=showurl)
 p.start()
